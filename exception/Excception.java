fce207e10cd1df1493d18f56e526b515042cbf09
Thristan Jay Nakila <136983420+FernnT@users.noreply.github.com>
2023-10-22 22:01:00 +0800

    Add files via upload

---
+++ exception/Excception.java
@@ -0,0 +1,18 @@
+package exception;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+//package excception;
+//
+///**
+// *
+// * @author TJ
+// */
+//public class Excception {
+//
+//    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+//    }
+//}
---
+++ exception/ShoppingCart.java
@@ -0,0 +1,13 @@
+package exception;
+public class ShoppingCart {
+
+    public static void main(String[] args) {
+        Calculator calc = new Calculator();
+        int addResult = calc.add(43, 79);
+        System.out.println("Add Result: " + addResult);
+
+        double divResult = calc.divide(15, 3);
+        System.out.println("Division Result: " + divResult);
+
+    }
+}
